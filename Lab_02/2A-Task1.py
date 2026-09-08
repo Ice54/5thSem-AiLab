89d0bc848ae445a2967dcf71ed13fe1550829600
Raahim Naveed <raahimnaveedbutt@gmail.com>
2026-09-08 00:27:13 -0700

    Lab_02 Added

---
+++ Lab_02/2A-Task1.py
@@ -0,0 +1,41 @@
+import math
+
+
+class basic_calc:
+    def __init__ (self,a,b):
+        self.a = a
+        self.b = b
+
+    def sum(self):
+        return self.a+self.b
+    def sub(self):
+        return self.a-self.b
+    def mul(self):
+        return self.a*self.b
+    def div(self):
+        if self.a == 0:
+            return 0
+        elif self.b == 0:
+            return 0
+        else:
+            return self.a/self.b
+
+class s_calc(basic_calc):
+    def __init__(self,a,b):
+        basic_calc.__init__(self,a,b)
+
+    def power(self):
+        return self.a**self.b
+
+    def log(self):
+        return math.log10(self.a)
+
+    def exp(self):
+        return math.exp(self.a)
+
+    def ln(self):
+        return math.log(self.a)
+
+    def factorial(self):
+        return math.factorial(self.a)
+
