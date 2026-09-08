77cf0f7a4c42f163adc01a08c5b8001551874d91
Raahim Naveed <raahimnaveedbutt@gmail.com>
2026-09-08 01:12:54 -0700

    Lab_02 Edited

--- Lab_02/2A-Task1.py
+++ Lab_02/2A-Task1.py
@@ -39,3 +39,17 @@ class s_calc(basic_calc):
     def factorial(self):
         return math.factorial(self.a)
 
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+
+x = s_calc(int(a),int(b))
+print("Sum =" + str(x.sum()))
+print("Sub =" + str(x.sub()))
+print("Multiply ="+str(x.mul()))
+print("Division ="+str(x.div()))
+print("Power ="+str(x.power()))
+print("Log ="+str(x.log()))
+print("Factorial ="+str(x.factorial()))
+print("Exponential ="+str(x.exp()))
+print("Natural Log ="+str(x.ln()))
+
